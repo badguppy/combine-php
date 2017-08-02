10edc845e08c9f1750ab88fe17215b70c5d2f19c
Soumik Chatterjee <badguppy@users.noreply.github.com>
2017-08-03 00:14:18 +0530

    Add files via upload

---
+++ demo/module/auth/user.php
@@ -0,0 +1,12 @@
+<?php 
+
+function func2() {
+	echo "<br/><b>func2 Called</b><br/>";
+		var_dump(func_get_args());
+		echo "<br/>";
+}
+
+echo "module loaded";
+
+
+?>
---
+++ demo/view/backend/admin/dash.new.php
@@ -0,0 +1,12 @@
+<?php 
+
+function func() {
+	echo "<br/><b>func Called</b><br/>";
+		var_dump(func_get_args());
+		echo "<br/>";
+}
+
+echo "view loaded";
+
+
+?>
